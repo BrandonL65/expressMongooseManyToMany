112a7df70207473dc8bcf3486400c344508bb884
Brandon <brandon.lau86@gmail.com>
2019-11-17 16:55:16 -0500

    Add many to many relationships for Groups

--- controllers/GroupController.js
+++ controllers/GroupController.js
@@ -10,5 +10,22 @@ module.exports = {
     let newGroup = new GroupModel(req.body);
     await newGroup.save();
     res.json(newGroup);
+  },
+  update: async (req,res) => {
+    let updatedGroup = await GroupModel.findOneAndUpdate({groupName: req.params.name}, req.body);
+    res.json(updatedGroup);
+  },
+  addUser: async (req,res) => {
+    let foundGroup = await GroupModel.find({groupName: req.params.name});
+    let foundUser = await UserModel.find({name: req.body.name});
+    foundGroup[0].users.push(foundUser[0].id);
+    foundUser[0].groups.push(foundGroup[0].id);
+    await foundGroup[0].save();
+    await foundUser[0].save();
+    res.json(foundGroup[0]);
+  },
+  seeUsers: async (req,res) => {
+    let groupWithUsers = await GroupModel.find({groupName: req.params.name}).populate("users");
+    res.json(groupWithUsers);
   }
 }
--- server.js
+++ server.js
@@ -20,11 +20,13 @@ app.get("/users", UserController.all);
 app.get("/users/create", UserController.create);
 app.get("/users/update/:name", UserController.update);
 app.get("/users/delete/:name", UserController.delete);
-app.get("/users/:name/addGroup", UserController.addGroup);
+app.post("/users/:name/addGroup", UserController.addGroup);
 app.get("/users/:name/seeGroups", UserController.seeGroups);
 const GroupController = require("./controllers/GroupController.js");
 app.get("/groups", GroupController.all);
 app.get("/groups/create", GroupController.create);
-
+app.get("/groups/:name/update", GroupController.update);
+app.post("/groups/:name/addUser", GroupController.addUser);
+app.get("/groups/:name/seeUsers", GroupController.seeUsers);
 //Connect to SERVER
 app.listen(PORT);
